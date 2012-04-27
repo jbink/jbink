cab1b7bd92128aa29674616e3d78f1ada2306e96
jbin K <admin@admin-PC>
2012-04-28 00:12:50 +0900

    2012-04-27 수정본

--- Gugudan/src/Chapter1/Gugudan.java
+++ Gugudan/src/Chapter1/Gugudan.java
@@ -6,10 +6,11 @@ public class Gugudan {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub     
+		// TODO Auto-generated method stub   
+		gugudan();
 
 	}
-	public void gugudan(){
+	public static void gugudan(){
 		for(int i=2 ; i<10 ; i++){
 			for(int j=1 ; j<10; j++){
 				System.out.println(i+" * "+j+ " = "+i*j );
